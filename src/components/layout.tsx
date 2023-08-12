fe521a4ce11a3bb56d3b40856b13a90f710a5ba9
Mutasem Salem <ms5268@drexel.edu>
2023-08-12 14:38:07 -0400

    Post Styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,14 @@
 import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
-    return(
-    <main className="h-screen flex justify-center">
-        <div className="md:w-full border-slate-400 h-full max-w-2xl border-x overflow-y-scroll"> 
-                {props.children}
-            </div>
-    </main>
-    );
-}
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <header className="bg-black border-b border-gray-200 py-2 px-4">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
+          <h1 className="text-xl font-semibold">Salem</h1>
+        </div>
+      </header>
+      <main className="max-w-3xl mx-auto py-4 px-2">{props.children}</main>
+    </div>
+  );
+};
--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -3,28 +3,63 @@ import dayjs from "dayjs";
 import Image from "next/image";
 import Link from "next/link";
 import relativeTime from "dayjs/plugin/relativeTime";
-
+import { useState } from "react";
+import styles from '../styles/CreatePostWizard.module.css';
 
 dayjs.extend(relativeTime);
-type PostWithUser = RouterOutputs["posts"]["getAll"][number]
-  export const PostView = (props :PostWithUser) => {
-    const {post,author} = props;
-    return (
-      <div key = {post.id} className="flex gap-3 border-b border-slate-400 p-4">
-        <Image src={author.profilePicture} className="w-12 h-12 rounded-full" alt={`@${author.username}'s profile picture`}
+import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+export const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+    
+  const handleLikeClick = () => {
+    if (!liked) {
+      setLikeCount(likeCount + 1);
+      setLiked(true);
+    }else {
+      setLikeCount(likeCount - 1);
+      setLiked(false);
+    }
+  };
+  
+  return (
+    <div className={`${styles.postBubble}`}>
+      <Image
+        src={author.profilePicture}
+        className="w-12 h-12 rounded-full"
+        alt={`@${author.username}'s profile picture`}
         width={56}
         height={56}
-        />
-        <div className="flex flex-col">
-          <div className="flex gap-1 text-slate-300 font-bold">
+      />
+      <div className="flex flex-col flex-grow">
+        <div className="flex items-center justify-between mb-2">
+          <div className="flex items-center gap-2">
             <Link href={`/@${author.username}`}>
-              <span>{`@${author.username}`}</span>
-              </Link>
-            <Link href={`/post/${post.id}`}><span className="font-thin">
-              {`- ${dayjs(post.createdAt).fromNow()}`}</span></Link>
+              <span className="font-semibold text-gray-500 hover:text-blue-400">{author.username}</span>
+            </Link>
+            <Link href={`/post/${post.id}`}>
+               <span className="text-gray-500">·</span>&nbsp;
+               <span className="text-gray-500 hover:text-blue-400">
+                {dayjs(post.createdAt).fromNow()}
+                </span>
+                </Link>
           </div>
-          <span className="text">{post.content}</span>
-          </div>   
+        </div>
+        <p className="text-gray-200">{post.content}</p>
+        <div className="flex gap-2 mt-2">    
+        <button
+          className="text-gray-500 hover:text-blue-400"
+          onClick={handleLikeClick}>
+            <FavoriteSharpIcon color={liked ? 'error' : 'inherit'} />
+          </button>
+          <span className="text-gray-500">{likeCount}</span>
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
